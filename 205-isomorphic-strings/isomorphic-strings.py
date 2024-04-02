56184c3218502bf7ef9f1016023727f92206a034
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-04-02 07:35:31 +0530

    Time: 69 ms (5.08%) | Memory: 16.7 MB (64.59%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,6 +2,8 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dict = {}
         dict2={}
+        if len(set(s)) != len(set(t)):
+            return False
         for i,j in zip(s,t):
             if i in dict.keys():
                 if dict[i]!=j:
