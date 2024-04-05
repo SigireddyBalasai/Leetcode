520dfc06c83cc24e872d53466ab7449b05bd1710
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-04-05 22:44:56 +0530

    Time: 33 ms (83.07%) | Memory: 16.6 MB (46.83%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        
+        for c in s:
+            if stack and c.swapcase() == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(c)
+        return "".join(stack)
