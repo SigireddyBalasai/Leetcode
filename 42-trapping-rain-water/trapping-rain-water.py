8725fa020d65534d8c2d7ef2d7b8cd059ab666e1
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-02 15:27:47 +0530

    Time: 2761 ms (5.00%) | Memory: 18.5 MB (55.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -6,6 +6,7 @@ class Solution:
         area = 0
         max_curr = 0
         arr2 = height.copy()
+        
         max_overall = max(arr2)
         for i in range(0,len(height)):
             if height[i] >= max_curr and 1+1 < len(height):
@@ -13,18 +14,18 @@ class Solution:
             if height[i] == max_overall:
                 max_ = max(height[(i+1):]+[0])
                 max_overall = max_
-            max_l = max_curr
+            # max_l = max_curr
 
-            # for j in range(i-1,-1,-1):
-            #     if height[j] > height[i] and height[j] > max_l and j >= 0:
-            #         max_l = height[j]
-            max_r = max_overall
-            # for j in range(i+1,len(height)):
-            #     if height[j] > height[i] and height[j] > max_r:
-            #         max_r = height[j]
-            ar =  min(max_l,max_r) - height[i]
+            # # for j in range(i-1,-1,-1):
+            # #     if height[j] > height[i] and height[j] > max_l and j >= 0:
+            # #         max_l = height[j]
+            # max_r = max_overall
+            # # for j in range(i+1,len(height)):
+            # #     if height[j] > height[i] and height[j] > max_r:
+            # #         max_r = height[j]
+            ar =  min(max_curr,max_overall) - height[i]
             if ar >= 0:
                 area += ar
 
-            print(max_l,height[i],max_r,area)        
+            # print(max_l,height[i],max_r,area)        
         return area
