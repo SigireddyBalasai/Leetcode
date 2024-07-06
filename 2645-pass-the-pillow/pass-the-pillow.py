800684a27419c6c44608f16f8b0f592f764e12d1
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-06 11:26:39 +0530

    Time: 34 ms (59.24%) | Memory: 16.6 MB (7.09%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -2,7 +2,7 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         # t = 0
         # upgrade = +1
-        b,a = divmod(time ,(n-1))
+        b,a = divmod(time,n-1)
         if b % 2 == 0:
             return 1+a
         return n - a
