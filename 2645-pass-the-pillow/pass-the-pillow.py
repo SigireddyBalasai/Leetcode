8847f24d4432bf30d299e8df75a58ea242056830
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-06 11:07:59 +0530

    Time: 43 ms (10.13%) | Memory: 16.7 MB (7.09%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -4,7 +4,10 @@ class Solution:
         upgrade = +1
         if time < n:
             return time + 1
-        print((time+1)%n,(time+1)//n)
+        print(time % (n-1),time // (n-1))
+        if (time // (n-1)) == 1:
+            return n - time // (n-1) - time % (n-1) + 1
+        # print((time+1)%n,(time+1)//n)
         for i in range(time+1):
             if t == n or (t == 1 and upgrade == -1):
                 upgrade = -upgrade
