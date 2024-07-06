270ac29ed3b690eedfb59a74830d55f6e5363b28
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-06 11:50:46 +0530

    Time: 55 ms (76.76%) | Memory: 17.6 MB (11.13%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -2,8 +2,9 @@ class Solution:
     def removeDuplicates(self, s: str) -> str:
         arr : List[str]= []
         for i in s:
-            if arr and i == arr[-1]:
+            if arr and arr[-1]==i:
                 arr.pop()
+                continue
             else:
                 arr.append(i)
         return "".join(arr)
