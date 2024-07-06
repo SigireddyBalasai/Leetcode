5154e7cda74345efd0849d2e3b81b76b226c0539
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-06 11:20:58 +0530

    Time: 38 ms (35.95%) | Memory: 16.5 MB (77.72%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -2,15 +2,15 @@ class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         t = 0
         upgrade = +1
-        if time < n:
-            return time + 1
-        print(time % (n-1),time // (n-1))
-        if (time // (n-1)) == 1:
-            return n - time // (n-1) - time % (n-1) + 1
-        # print((time+1)%n,(time+1)//n)
-        for i in range(time+1):
-            if t == n or (t == 1 and upgrade == -1):
-                upgrade = -upgrade
-            t += upgrade
-            print(t,i)
-        return t
+        a = time % (n-1)
+        b = time // (n-1)
+        if b % 2 == 0:
+            return 1+a
+        else:
+            return n - a
+        # for i in range(time+1):
+        #     if t == n or (t == 1 and upgrade == -1):
+        #         upgrade = -upgrade
+        #     t += upgrade
+        #     print(t,i)
+        # return t
