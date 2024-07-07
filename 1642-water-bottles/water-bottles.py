8b7cea67852cd8246cd82485af201d5ae15517ee
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-07 11:57:47 +0530

    Time: 37 ms (38.61%) | Memory: 16.6 MB (32.78%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        sum=0
+        num_full=numBottles
+        num_empty=0
+        while(num_full>0):
+            sum+=num_full
+            num_empty+=num_full
+            num_full=0
+            num_change = math.floor(num_empty/numExchange)
+            num_full=num_change
+            num_empty-=num_change*numExchange
+            print(num_full)
+        print(sum)
+        return sum
+        
