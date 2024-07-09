87cb1c04d860abf9504b7093dec29e152c945ce0
SigireddyBalasai <sigireddybalasai@gmail.com>
2024-07-09 11:12:14 +0530

    Time: 707 ms (42.50%) | Memory: 56.1 MB (80.00%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def averageWaitingTime(self, customers):
+        """
+        :type customers: List[List[int]]
+        :rtype: float
+        """
+        sum = 0.0
+        time = customers[0][0]
+        count=0
+        for i in customers:
+            time=max(i[0],time)
+            time += i[1]
+            ok = (time-i[0])
+            # print(ok,time,i)
+            sum += ok
+            count+=1
+        # print(len(customers),sum)
+        return (sum/count)
